578f0dd30fc75f9a430fbcb578e50867a8d9f46d
Steve McGrane <temporarilyoffline@gmail.com>
2019-11-18 18:44:09 -0600

    more django like

--- manage.py
+++ manage.py
@@ -1,5 +1,38 @@
 #!/usr/bin/env python
-import os
-import sys
+#import os
+#import sys
 
-print('Hello World')
+#print('Hello World')
+
+
+import textwrap
+
+from six.moves.BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class HelloRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path != '/':
+            self.send_error(404, "Object not found")
+            return
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        response_text = textwrap.dedent('''\
+            <html>
+            <head>
+                <title>Greetings to the world</title>
+            </head>
+            <body>
+                <h1>Greetings to the world</h1>
+                <p>Hello, world!</p>
+            </body>
+            </html>
+        ''')
+        self.wfile.write(response_text.encode('utf-8'))
+
+
+server_address = ('', 8000)
+httpd = HTTPServer(server_address, HelloRequestHandler)
+httpd.serve_forever()
